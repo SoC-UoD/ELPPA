261643fdbe048b14f79c5c4b73d6753ba9cf9337
James Oliver <jwzoliver@dundee.ac.uk>
2012-10-07 22:38:29 +0100

    Implemented Wait Dialogue
    User wait dialogue displays on screen while data is downloaded.

--- StockTracker/gen/com/elppa/stocktracker/R.java
+++ StockTracker/gen/com/elppa/stocktracker/R.java
@@ -20,7 +20,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080001;
+        public static final int menu_run1=0x7f080001;
+        public static final int menu_run2=0x7f080002;
+        public static final int menu_run3=0x7f080003;
         public static final int testButton=0x7f080000;
     }
     public static final class layout {
@@ -32,7 +34,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
+        public static final int menu_run1=0x7f050002;
+        public static final int menu_run2=0x7f050004;
+        public static final int menu_run3=0x7f050005;
         public static final int title_activity_home=0x7f050003;
     }
     public static final class style {
--- StockTracker/src/com/elppa/stocktracker/Home.java
+++ StockTracker/src/com/elppa/stocktracker/Home.java
@@ -4,6 +4,8 @@ package com.elppa.stocktracker;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,47 +21,62 @@ import com.elppa.webserviceaccess.FetchYahooData;
 public class Home extends Activity 
 {
 	private ShareDetailsObject [] ShareDetails = null;
-
+	private Button testButton = null;
+	private AccessWebserviceTask AWT = null;
+	
+	/**
+	 * Initialise GUI Elements
+	 * 
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         
-        Button testButton = (Button) this.findViewById(R.id.testButton);
+        testButton = (Button) this.findViewById(R.id.testButton);
         
-        testButton.setOnClickListener(new View.OnClickListener() 
-        {
-			@Override
-			public void onClick(View v) 
-			{
-				//Check to see if we already have data, if we do, let's quit.
-				//Implement a timer so that refreshes take place on a 5 minute interval
-				if(ShareDetails != null)
-					return;
-				
-				// TODO Auto-generated method stub
-				AccessWebserviceTask AWT = new AccessWebserviceTask();
-				
-				Log.i(Home.class.toString(), "Executing Web Service Task");
-				
-				//Create arrays with the details required.
-				// TODO Create a static object with required share data.
-							
-				String [] columns = {"*"};
-	    		String [] symbols = {"bp.l"};
-			
-	    		//Create AccessWebserviceTask thread - this method calls doInBackground and passes string arrays with required details.
-	    		//
-				AWT.execute(columns, symbols);
-			}
-		});
+		//Check to see if we already have data, if we do, let's quit.
+		//Implement a timer so that refreshes take place on a 5 minute interval
+		if(ShareDetails != null)
+			return;
+		
+		AWT = new AccessWebserviceTask(Home.this);
+		
+		Log.i(Home.class.toString(), "Executing Web Service Task");
+		
+		//Create arrays with the details required.
+		// TODO Create a static object with required share data.
+					
+		String [] columns = {"*"};
+		String [] symbols = {"bp.l"};
+	
+		//Create AccessWebserviceTask thread - this method calls doInBackground and passes string arrays with required details.
+		//
+		AWT.execute(columns, symbols);
+    }
+    
+    /**
+     * This method is called before the activity appears on screen.  Initialise event handlers etc.
+     * 
+     */
+    @Override
+    protected void onStart() 
+    {
+        super.onStart();
+       
         
+    }
+    
+    @Override
+    protected void onPause()
+    {
+    	super.onPause();
     }
     
     private boolean checkShareVolumeForRun()
     {
-    	
+    	testButton.setText(ShareDetails[0].getSymbol());
     	
     	return false;
     }
@@ -76,20 +93,26 @@ public class Home extends Activity
     * @author James Oliver
     * 
     * This class create a new thread separate from the UI.  Input into the class is: String[] (columns and symbols required from Yahoo) and returns a ShareDetailsObject[] array.
-    * 
-    * String[] is the input into doInBackgound()
-    * ShareDetailsObject[] is returned from onPostExecute()
-    * 
+    * Constructor requires the current activity context to display a refreshing dialogue.
     *
     */
-
-    
     protected class AccessWebserviceTask extends AsyncTask<String[], Void, ShareDetailsObject []>
     {
-    	public AccessWebserviceTask()
+    	private ProgressDialog pd = null;
+    	private Context cxt = null;
+    	private FetchYahooData FYD = null;
+    	
+    	/**
+    	 * 
+    	 * Default constructor, requires the current activity to display refresh dialogue (Activity.this)
+    	 * 
+    	 * @param context
+    	 */
+    	public AccessWebserviceTask(Context context)
     	{
     		super();
-    	
+    		
+    		cxt = context;
     	}
     	
     	@Override
@@ -101,27 +124,53 @@ public class Home extends Activity
     	protected ShareDetailsObject[] doInBackground(String[]... params) 
     	{
     		// TODO Auto-generated method stub
-    		FetchYahooData FYD = new FetchYahooData();
+    		FYD = new FetchYahooData();
     		
     		String[] columns = params[0];
     		String[] symbols = params[1];
-    		
+    		    		
     		ShareDetailsObject [] ShareDetails = null;
 
     		ShareDetails = FYD.DownloadCurrentYahooData(columns, symbols);
-
+    		
     		return ShareDetails;
+    	}
+    	
+    	protected void onPreExecute()
+    	{
+    		pd = new ProgressDialog(cxt);
+    		pd.setTitle("Refreshing Data");
+    		pd.setMessage("Downloading New Data, please wait...");
+    		pd.setCancelable(true);
+    		pd.setIndeterminate(false); pd.setProgressStyle(ProgressDialog.STYLE_SPINNER); pd.setMax(5);
+    		pd.show();
     	}
     	
     	protected void  onProgressUpdate()
     	{
-    		Toast.makeText(getApplicationContext(), "Please Wait", Toast.LENGTH_SHORT).show();
+    		//pd.setProgress(0);
     	}
     	
     	protected void onPostExecute(ShareDetailsObject [] result)
     	{
+
     		if(result != null)
+    		{
     			ShareDetails = result;
+    			Home.this.checkShareVolumeForRun();
+    			pd.dismiss();
+    		}else
+    		{
+    			pd.setTitle("Download Error");
+    			pd.setMessage("Unable to download finance data, please refresh in a few moments");
+    			try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    			pd.dismiss();
+    		}	
     	}
     }
 }
